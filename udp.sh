8b6b462e05e74ec4544d479a0828ad37b1401f9e
BaeGyee9 <zawtunwaiz@gmail.com>
2026-05-18 03:15:47 +0700

    Update udp.sh

--- udp.sh
+++ udp.sh
@@ -230,10 +230,10 @@ if jq . >/dev/null 2>&1 <<<'{}'; then
   jq --argjson pw "$PW_LIST" --arg ip "$SERVER_IP" '
     .auth.mode = "passwords" |
     .auth.config = $pw |
-    .listen = (."listen" // ":5667") |
+    .listen = ":5667" |
     .cert = "/etc/zivpn/zivpn.crt" |
     .key  = "/etc/zivpn/zivpn.key" |
-    .obfs = (."obfs" // "zivpn") |
+    .obfs = "wechat" |
     .server = $ip
   ' "$CFG" > "$TMP" && mv "$TMP" "$CFG"
 fi
@@ -1605,6 +1605,7 @@ IFACE=$(ip -4 route ls | awk '/default/ {print $5; exit}')
 # DNAT Rules
 iptables -t nat -F
 iptables -t nat -A PREROUTING -i "$IFACE" -p udp --dport 6000:19999 -j DNAT --to-destination :5667
+iptables -t nat -A PREROUTING -i "$IFACE" -p udp --dport 5667 -j DNAT --to-destination :5667
 iptables -t nat -A POSTROUTING -o "$IFACE" -j MASQUERADE
 
 # UFW Rules
